ef441e1cb822d7799bcfa94e1c0e305f774d6dee
ItzABC <32772245+ItzABC@users.noreply.github.com>
2018-09-01 15:10:13 +0800

    Add files via upload

---
+++ CppCyaronGraph.h
@@ -0,0 +1,113 @@
+/*
+    This Project "Project CppCyaron" is created for people
+    who want to generate many data files without using
+    "CyaRon" by LuoGu Dev-Team.
+    This file is a part of "Project CppCyaron".
+
+    you can redistribute it and/or modify it under the
+    terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 3, or (at your option)
+    any later version.
+
+    Copyright(C) 2018~2018, GD_ItzABC(Kirisame_Marisa_ sometimes)
+*/
+#ifndef CppCyaronGraph
+#define CppCyaronGraph
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<cctype>
+#include<cstdlib>
+#include<ctime>
+#include<cstdarg>
+#include<climits>
+#include<random>
+#include<vector>
+#include<algorithm>
+#include"CppCyaronMath.h"
+#pragma GCC optimize("O2")
+using std::vector;
+namespace CppCyaronGraphFunctions
+{
+    struct __Edge
+    {
+        int32_t __from,__to;
+        __Edge(){__from=__to=0;}
+        __Edge(int32_t __GivenFrom_Point,int32_t __GivenTo_Point){__from=__GivenFrom_Point;__to=__GivenTo_Point;}
+        ~__Edge(){delete &__from;delete &__to;}
+    };
+    struct __WeightedEdge
+    {
+        int32_t __from,__to;
+        uint64_t __weight;
+        __WeightedEdge(){__from=__to=__weight=0;}
+        __WeightedEdge(int32_t __GivenFrom_Point,int32_t __GivenTo_Point,uint64_t __GivenWeight){__from=__GivenFrom_Point;__to=__GivenTo_Point;__weight=__GivenWeight;}
+        ~__WeightedEdge(){delete &__from;delete &__to;delete &__weight;}
+    };
+    struct __Graph
+    {
+        int32_t __vertex_amount;
+        vector<__Edge>__edgelist;
+        __Graph(){__vertex_amount=0;__edgelist.clear();}
+        ~__Graph(){delete &__vertex_amount;__edgelist.clear();delete &__edgelist;}
+        void add(__Edge __added_edge){__edgelist.push_back(__added_edge);}
+        void SpawnGraph(int32_t __vertex_count,int32_t __edge_count,bool __self_edges,bool __repeat_edges,bool __directed)
+        {
+            using namespace CppCyaronMathFunctions;
+            __vertex_amount=__vertex_count;
+            vector<int>__vec[__vertex_count+1];
+            for(int i=1;i<=__vertex_count;++i)__vec[i].push_back(i);
+            for(int32_t i=1;i<=__edge_count;++i)
+            {
+                int32_t __from_vertex,__to_vertex;
+                while(1)
+                {
+                    __from_vertex=rand_int32(1,__vertex_count);
+                    __to_vertex=rand_int32(1,__vertex_count);
+                    if(!__self_edges&&__from_vertex==__to_vertex)continue;
+                    if(__self_edges&&__from_vertex==__to_vertex)
+                    {
+                        __edgelist.push_back(__Edge(__from_vertex,__to_vertex));
+                        std::sort(__vec[__from_vertex].begin(),__vec[__from_vertex].end());
+                        if(!__directed)
+                        {
+                            __edgelist.push_back(__Edge(__to_vertex,__from_vertex));
+                            std::sort(__vec[__to_vertex].begin(),__vec[__to_vertex].end());
+                        }
+                        continue;
+                    }
+                    if(!__repeat_edges&&binary_search(__vec[__from_vertex].begin(),__vec[__from_vertex].end(),__to_vertex))
+                    {
+                        /*need to fix the problem of self edges*/
+                        bool __added=0;
+                        for(int j=0;j<__vec[__from_vertex].size();++j)
+                        {
+                            if(__vec[__from_vertex][j]!=j+1)
+                            {
+                                __added=1;
+                                __to_vertex=j+1;
+                                break;
+                            }
+                        }
+                        if(!__added)continue;
+                    }
+
+                    __edgelist.push_back(__Edge(__from_vertex,__to_vertex));
+                    __vec[__from_vertex].push_back(__to_vertex);
+                    std::sort(__vec[__from_vertex].begin(),__vec[__from_vertex].end());
+                    if(!__directed)
+                    {
+                        __edgelist.push_back(__Edge(__to_vertex,__from_vertex));
+                        __vec[__to_vertex].push_back(__from_vertex);
+                        std::sort(__vec[__to_vertex].begin(),__vec[__to_vertex].end());
+                    }
+                    break;
+                }
+            }
+        }
+    };
+}
+
+#endif // CppCyaronGraph
---
+++ CppCyaronMath.h
@@ -0,0 +1,128 @@
+/*
+    This Project "Project CppCyaron" is created for people
+    who want to generate many data files without using
+    "CyaRon" by LuoGu Dev-Team.
+    This file is a part of "Project CppCyaron".
+
+    you can redistribute it and/or modify it under the
+    terms of the GNU General Public License as published by the
+    Free Software Foundation; either version 3, or (at your option)
+    any later version.
+
+    Copyright(C) 2018~2018, GD_ItzABC(Kirisame_Marisa_ sometimes)
+*/
+#ifndef CppCyaronMath
+#define CppCyaronMath
+
+#include<iostream>
+#include<cstdio>
+#include<cstring>
+#include<cmath>
+#include<cctype>
+#include<cstdlib>
+#include<ctime>
+#include<cstdarg>
+#include<climits>
+#include<random>
+#pragma GCC optimize("O2")
+namespace CppCyaronMathFunctions
+{
+        //some math functions
+    template<typename T> T gcd(T __a__gcd_parameter,T __b__gcd_parameter)
+    {
+        while(__b__gcd_parameter)
+        {
+            __a__gcd_parameter%=__b__gcd_parameter;
+            std::swap(__a__gcd_parameter,__b__gcd_parameter);
+        }
+        return __a__gcd_parameter;
+    }
+    template<typename T> T abs(const T __parameter)
+    {
+        return __parameter>0?__parameter:-__parameter;
+    }
+
+    uint64_t __lastseed=1954136;
+
+    uint64_t __rand_uint64(){return ((uint64_t)rand()<<62)+((uint64_t)rand()<<30)+((uint64_t)rand()<<15)+((uint64_t)rand());}
+    int64_t __rand_int64(){return ((int64_t)rand()<<62)+((int64_t)rand()<<30)+((int64_t)rand()<<15)+((int64_t)rand());}
+    uint32_t __rand_uint32(){return ((uint32_t)rand()<<30)+((uint32_t)rand()<<15)+((uint32_t)rand());}
+    int32_t __rand_int32(){return ((int32_t)rand()<<30)+((int32_t)rand()<<15)+((int32_t)rand());}
+    /**
+    * These functions(rand_int32,rand_uint32,rand_int64,rand_uint64)
+    * are used for spawning an integer in the range of [lower,upper].
+    */
+    int32_t rand_int32(int32_t __lower_limit=INT_MIN,int32_t __upper_limit=INT_MAX)
+    {
+        int32_t __random_value;
+        srand(time(0)+(int32_t)ceil(clock()*100000)-__random_value^__lastseed);
+        __random_value=(__lastseed^=__rand_int32());
+        if(__upper_limit<__lower_limit)
+        {
+            fprintf(stderr,"Upper limit is less than the lower limit.\n");
+            return 1;
+        }
+        if(__upper_limit-__lower_limit+1==0)return __random_value;
+        return __lower_limit+abs(__random_value%(__upper_limit-__lower_limit+1));
+    }
+    /**
+    * These functions(rand_int32,rand_uint32,rand_int64,rand_uint64)
+    * are used for spawning an integer in the range of [lower,upper].
+    */
+    uint32_t rand_uint32(uint32_t __lower_limit=0,uint32_t __upper_limit=UINT_MAX)
+    {
+        uint32_t __random_value;
+        srand(time(0)+(uint32_t)ceil(clock()*100000)-__random_value^__lastseed);
+        __random_value=(__lastseed^=__rand_uint32());
+        if(__upper_limit<__lower_limit)
+        {
+            fprintf(stderr,"Upper limit is less than the lower limit.\n");
+            return 1;
+        }
+        if(__upper_limit-__lower_limit+1==0)return __random_value;
+        return __lower_limit+abs(__random_value%(__upper_limit-__lower_limit+1));
+    }
+    /**
+    * These functions(rand_int32,rand_uint32,rand_int64,rand_uint64)
+    * are used for spawning an integer in the range of [lower,upper].
+    */
+    int64_t rand_int64(int64_t __lower_limit=LLONG_MIN,int64_t __upper_limit=LLONG_MAX)
+    {
+        int64_t __random_value;
+        srand(time(0)+(int64_t)ceil(clock()*100000)-__random_value^__lastseed);
+        __random_value=(__lastseed^=__rand_int64());
+        if(__upper_limit<__lower_limit)
+        {
+            fprintf(stderr,"Upper limit is less than the lower limit.\n");
+            return 1;
+        }
+        if(__upper_limit-__lower_limit+1==0)return __random_value;
+        return __lower_limit+abs(__random_value%(__upper_limit-__lower_limit+1));
+    }
+    /**
+    * These functions(rand_int32,rand_uint32,rand_int64,rand_uint64)
+    * are used for spawning an integer in the range of [lower,upper].
+    */
+    uint64_t rand_uint64(uint64_t __lower_limit=0,uint64_t __upper_limit=ULLONG_MAX)
+    {
+        uint64_t __random_value;
+        srand(time(0)+(uint64_t)ceil(clock()*100000)-__random_value^__lastseed);
+        __random_value=(__lastseed^=__rand_uint64());
+        if(__upper_limit<__lower_limit)
+        {
+            fprintf(stderr,"Upper limit is less than the lower limit.\n");
+            return 1;
+        }
+        if(__upper_limit-__lower_limit+1==0)return __random_value;
+        return __lower_limit+abs(__random_value%(__upper_limit-__lower_limit+1));
+    }
+
+    double rand_double(double __lower_limit,double __upper_limit)
+    {
+        std::default_random_engine e(time(0)+(uint32_t)ceil(clock()*100000)^__lastseed);
+        std::uniform_real_distribution<double> u(__lower_limit,__upper_limit);
+        __lastseed^=(int32_t)u(e);
+        return u(e);
+    }
+}
+#endif // CppCyaronMath
